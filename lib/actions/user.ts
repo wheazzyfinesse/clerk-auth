4e293d7cf3dfc2269bcad7961432df426ea1e592
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2025-01-07 18:14:32 +0100

    first commit

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -5,8 +5,8 @@ import { connectDB, connectMongoDB } from "../mongodb";
 
 export const createUser = async (user: any) => {
 	try {
-		// await connectDB();
-		await connectMongoDB();
+		await connectDB();
+		// await connectMongoDB();
 		const newUser = await User.create(user);
 		return JSON.parse(JSON.stringify(newUser));
 	} catch (error) {
